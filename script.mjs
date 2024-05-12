b632799eaebaebcb02096b7ed56b1546ee42dcb8
bbcy777 <bbcy777@gmail.com>
2024-05-11 21:21:45 -0700

    part 1 -3

--- script.mjs
+++ script.mjs
@@ -1,84 +1,84 @@
-// //Part 1: Thinking Functionally
+//Part 1: Thinking Functionally
 
-// // Take an array of numbers and return the sum.
-// // Take an array of numbers and return the average.
-// // Take an array of strings and return the longest string.
-// // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+// Take an array of numbers and return the sum.
+// Take an array of numbers and return the average.
+// Take an array of strings and return the longest string.
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 
-// function arrayNumSum(array) {
-//     let sum = 0;
-//     for (let i = 0; i<array.length; i++) {
-//         sum += array[i];
-//     }
-//     return sum;
-// }
+function arrayNumSum(array) {
+    let sum = 0;
+    for (let i = 0; i<array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
 
-// function arrayNumAvg(array) {
-//     return(arrayNumSum(array)/array.length);
-// }
+function arrayNumAvg(array) {
+    return(arrayNumSum(array)/array.length);
+}
 
-// // let array = [2, 4, 6, 8];
-// // console.log(arrayNumAvg(array));
+// let array = [2, 4, 6, 8];
+// console.log(arrayNumAvg(array));
 
-// function arrayStr(arrayS) {
-//     let len = 0;
-//     for (let i = 0; i < arrayS.length; i++) {
-//         if (arrayS[i].length > len) {
-//             len = arrayS[i];
-//         }
-//     }
-//     return len;
-// }
+function arrayStr(arrayS) {
+    let len = 0;
+    for (let i = 0; i < arrayS.length; i++) {
+        if (arrayS[i].length > len) {
+            len = arrayS[i];
+        }
+    }
+    return len;
+}
 
-// function arrayLen(arrayS, num) {
-//     new_array = [];
-//     for (let str of arrayS) {
-//         if (str.length > num) {
-//             new_array.push(str);
-//         }
-//     }
-//     return new_array;
-// }
-// //Take a number, n, and print every number between 1 and n without using loops. Use recursion.
-// function list_num(num) {
-//     if (num > 1) {
-//         list_num(num - 1);
-//     }
-//     return console.log(num);
-// }
+function arrayLen(arrayS, num) {
+    new_array = [];
+    for (let str of arrayS) {
+        if (str.length > num) {
+            new_array.push(str);
+        }
+    }
+    return new_array;
+}
+//Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+function list_num(num) {
+    if (num > 1) {
+        list_num(num - 1);
+    }
+    return console.log(num);
+}
 
-// //Part 2: Thinking Methodically
-// // When functions are built into objects, like Arrays, they are referred to as “methods” of those objects. Many methods, including Array methods, require “callback functions” to determine their behavior.
-// // For the tasks below, use the following data to test your work:
+//Part 2: Thinking Methodically
+// When functions are built into objects, like Arrays, they are referred to as “methods” of those objects. Many methods, including Array methods, require “callback functions” to determine their behavior.
+// For the tasks below, use the following data to test your work:
 let people = [{ id: "42", name: "Bruce", occupation: "Knight" },
     { id: "48", name: "Barry", occupation: "Runner"},
     { id: "57", name: "Bob", occupation: "Fry Cook"},
     { id: "63", name: "Blaine", occupation: "Quiz Master"},
     { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
-// // Use callback functions alongside Array methods to accomplish the following:
-// // Sort the array by age.
-// function sortAge (a, b) {
-//     return parseInt(a.age) - parseInt(b.age);
-// }
-// people.sort(sortAge);
+// Use callback functions alongside Array methods to accomplish the following:
+// Sort the array by age.
+function sortAge (a, b) {
+    return parseInt(a.age) - parseInt(b.age);
+}
+people.sort(sortAge);
 
-// // Filter the array to remove entries with an age greater than 50.
-// people = people.filter(obj => parseInt(obj.age)<= 50);
+// Filter the array to remove entries with an age greater than 50.
+people = people.filter(obj => parseInt(obj.age)<= 50);
 
-// // Map the array to change the “occupation” key to “job” and increment every age by 1.
-// people.forEach((row) => {
-//     row.job = row.occupation;
-//     delete row.occupation;
-//     row.age = parseInt(row.age) + 1;
-// })
-// // console.log(people)
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+people.forEach((row) => {
+    row.job = row.occupation;
+    delete row.occupation;
+    row.age = parseInt(row.age) + 1;
+})
+// console.log(people)
 
-// // Use the reduce method to calculate the sum of the ages.
-// // Then use the result to calculate the average age.
-// let sum = people.reduce((accumulator, currentValue) => {return accumulator + currentValue.age; }, 0);
+// Use the reduce method to calculate the sum of the ages.
+// Then use the result to calculate the average age.
+let sum = people.reduce((accumulator, currentValue) => {return accumulator + currentValue.age; }, 0);
 
-// let avgAge = sum / people.length;
-// console.log(avgAge);
+let avgAge = sum / people.length;
+console.log(avgAge);
 
 // Part 3: Thinking Critically
 // It is important to remember that when working with objects in JavaScript, we can either pass those objects into functions by value or by reference. This important distinction changes the way that functions behave, and can have large impacts on the way a program executes.
